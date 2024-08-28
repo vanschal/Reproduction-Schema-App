3d4d686a54ccb4d4d39b952889c3bcad56add59a
vanschal <vanschal@bc.edu>
2024-08-28 17:08:19 -0400

    new changes big improvements

--- venvs/Dash-App-Version/firstDraft.py
+++ venvs/Dash-App-Version/firstDraft.py
@@ -11,10 +11,16 @@ import numpy as np
 #In the next draft I aim to link the input boxes and the slider values so that they correspond and match up with one another
 #The reset view button and balanced growth button will also be made functional
 
-
+#27/8/2024
 #I have the balanced growth button working now
 #need some clarification on the reset view button and what it has to achieve exactly
 #some help linking the input boxes and the slider values would also help greatly
+
+#28/8/2024
+#I improved the growth path buttons to make it more relevant 
+#gives more options for the user to either change k_1 or k_2 depending on what they want
+#The reset view button also works now
+#only obvious thing left for me to consider is now linking the input buttons to the slider values and vice versa
 
 app = dash.Dash(__name__)
 
@@ -35,9 +41,9 @@ app.layout = html.Div([
 
                 #slider and input box for rate of exploitation
             html.Div([
-                html.Label(" Rate of exploitation e:"),
+            html.Label(" Rate of exploitation e:"),
             dcc.Slider(id='slider-e', min=0, max=10, step=None, value=1, marks=None, tooltip={"placement": "bottom", "always_visible": True}),
-            dcc.Input(id='input-e', type='number', min=0, max=10, step=0.1, value=1, style={'marginLeft': '2px', 'width': '20px'})
+            dcc.Input(id='input-e', type='number', min=0, max=10, step=0.1, value=1, style={'marginLeft': '2px', 'width': '40px'})
             ],style={'alignItems': 'center'}),
 
             html.Br(),
@@ -48,7 +54,7 @@ app.layout = html.Div([
             html.Div([
             html.Label("Composition of Capital for Department 1 k_1:"),
             dcc.Slider(id='slider-k1', min=0, max=10, step=None, value=1.5, marks=None, tooltip={"placement": "bottom", "always_visible": True}),
-             dcc.Input(id='input-k1', type='number', min=0, max=10, step=0.1, value=1, style={'marginLeft': '2px', 'width': '20px'})
+             dcc.Input(id='input-k1', type='number', min=0, max=10, step=0.1, value=1, style={'marginLeft': '2px', 'width': '40px'})
             ],style={'alignItems': 'center'}),
 
             html.Br(),
@@ -59,7 +65,7 @@ app.layout = html.Div([
              html.Div([
             html.Label("Composition of Capital for Department 2 k_2:"),
             dcc.Slider(id='slider-k2', min=0, max=10, step=None, value=3.2, marks=None, tooltip={"placement": "bottom", "always_visible": True}),
-             dcc.Input(id='input-k2', type='number', min=0, max=10, step=0.1, value=1, style={'marginLeft': '2px', 'width': '20px'})
+             dcc.Input(id='input-k2', type='number', min=0, max=10, step=0.1, value=1, style={'marginLeft': '2px', 'width': '40px'})
             ],style={'alignItems': 'center'}),
 
             html.Br(),
@@ -70,7 +76,7 @@ app.layout = html.Div([
              html.Div([
             html.Label("Rate of reinvestment a:"),
             dcc.Slider(id='slider-a', min=0, max=0.99, step=None, value=0.5, marks=None, tooltip={"placement": "bottom", "always_visible": True}),
-             dcc.Input(id='input-a', type='number', min=0, max=0.99, step=0.1, value=0.5, style={'marginLeft': '2px', 'width': '20px'})
+             dcc.Input(id='input-a', type='number', min=0, max=0.99, step=0.1, value=0.5, style={'marginLeft': '2px', 'width': '40px'})
             ],style={'alignItems': 'center'}),
 
 
@@ -79,7 +85,7 @@ app.layout = html.Div([
             html.Div([
             html.Label("Initial Value Output for Department 1 y_1i:"),
             dcc.Slider(id='slider-y1i', min=0, max=10, step=None, value=1, marks=None, tooltip={"placement": "bottom", "always_visible": True}),
-            dcc.Input(id='input-y1i', type='number', min=0, max=10, step=0.1, value=1, style={'marginLeft': '2px', 'width': '20px'})
+            dcc.Input(id='input-y1i', type='number', min=0, max=10, step=0.1, value=1, style={'marginLeft': '2px', 'width': '40px'})
              ],style={'alignItems': 'center'}),
 
 
@@ -88,7 +94,7 @@ app.layout = html.Div([
             html.Div([
             html.Label("Initial Value Output for Department 2 y_2i:"),
             dcc.Slider(id='slider-y2i', min=0, max=10, step=None, value=1, marks=None, tooltip={"placement": "bottom", "always_visible": True}),
-            dcc.Input(id='input-y2i', type='number', min=0, max=10, step=0.1, value=1, style={'marginLeft': '2px', 'width': '20px'})
+            dcc.Input(id='input-y2i', type='number', min=0, max=10, step=0.1, value=1, style={'marginLeft': '2px', 'width': '40px'})
              ],style={'alignItems': 'center'}),
 
 
@@ -96,8 +102,27 @@ app.layout = html.Div([
                 #the eventual function of this is to set k1 and k2 equal to one another
                 #balanced growth, just like in the desmos app
              html.Button(
-                'Balanced Growth', 
-                id='balanced-growth-button',
+                'Balanced Growth K1 (adjusts K2 to equal K1)', 
+                id='balanced-growth-button-k1',
+                n_clicks=0,
+                style={
+                    'backgroundColor': '#28a745',  # Green background
+                    'color': 'white',  # White text
+                    'border': 'none',  # No border
+                    'padding': '10px 20px',  # Padding
+                    'textAlign': 'center',
+                    'textDecoration': 'none', 
+                    'display': 'inline-block',
+                    'fontSize': '16px',  # Font size
+                    'margin': '10px 0',  # Margin
+                    'cursor': 'pointer',  # Pointer cursor on hover
+                    'borderRadius': '5px',  # Rounded corners
+                }
+            ),
+
+                  html.Button(
+                'Balanced Growth K2 (adjusts K1 to equal K2)', 
+                id='balanced-growth-button-k2',
                 n_clicks=0,
                 style={
                     'backgroundColor': '#28a745',  # Green background
@@ -150,7 +175,7 @@ app.layout = html.Div([
                     'scrollZoom': True,
                     'displayModeBar': True,
                     'displaylogo': False,
-                   'modeBarButtonsToRemove': ['zoom2d','autoscale'],
+                   'modeBarButtonsToRemove': ['zoom2d','autoscale','resetscale2d','pan','zoomin2d','zoomout2d','toImage'],
                 }
             ),
         ], style={'width': '70%'}),
@@ -162,8 +187,13 @@ app.layout = html.Div([
 #  When a value changes the callback will be activated to update what the graph looks like according to the new values
 @app.callback(
     Output('graph', 'figure'),
-    Output('balanced-growth-button', 'n_clicks'), #giving it an output so that it can be reset to 0 as described below
-    # Output('reset-view-button','n_clicks'),
+    Output('balanced-growth-button-k1', 'n_clicks'),
+    Output('balanced-growth-button-k2', 'n_clicks'),
+    #giving it an output so that it can be reset to 0 as described below
+    #this is here because of the balanced growth button to link them up and reflect it on the slider value
+    Output('slider-k1','value'),
+    Output('slider-k2','value'),
+
 
     #below are the values used to get the initial graph by feeding them into the equations
     Input('slider-e','value'),
@@ -172,17 +202,18 @@ app.layout = html.Div([
     Input('slider-a', 'value'),
     Input('slider-y1i', 'value'),
     Input('slider-y2i', 'value'),
-
-    Input('balanced-growth-button', 'n_clicks'), #tracking if balanced growth path button has been clicked
+    Input('balanced-growth-button-k1', 'n_clicks'),
+    Input('balanced-growth-button-k2', 'n_clicks'),
+    Input('reset-view-button','n_clicks')
+    #tracking if balanced growth path button has been clicked
     # Input('reset-view-button', 'n_clicks') #tracking if rest view button has been clicked
 )
 
 
 # Update graph function, basically copied off of the tkinter version
-def update_graph(e, k_1, k_2, a, y_1i, y_2i, clicks_bg):
-    # Calculate variables based on slider values
+def update_graph(e, k_1, k_2, a, y_1i, y_2i, clicks_k1, clicks_k2, clicks_rv):
 
-    # if clicks_rv > 0:
+
 
     c_1 = k_1 / (1 + e + k_1)
     v_1 = (1 - c_1) / (1 + e)
@@ -213,13 +244,29 @@ def update_graph(e, k_1, k_2, a, y_1i, y_2i, clicks_bg):
 
     t_range = np.linspace(-3, 14, 1000)
 
+    sliderk1 = k_1
+    sliderk2 = k_2
+
+    #sets the initial value for the sliders to be returned later in case the user doesn't click the growth path buttons at all
+
 
     #if the user clicks the balanced growth button, they would set both of these values equal to one another achieving the
     #desired balanced growth, clicks is then set back to 0 to make the change impermanent and the cycle can repeat
 
-    if clicks_bg > 0:
-        k_1,k_2 = 2.5,2.5
-        clicks_bg = 0
+    #sets one value on to the other depending on which was clicked on the app below
+    #different if clause is activated depending on k1 or k2 click
+
+    if clicks_k1 > 0:
+        temp = k_1
+        k_2 = temp
+        sliderk2, sliderk1 = temp, temp
+        clicks_k1 = 0
+    
+    if clicks_k2 > 0:
+        temp = k_2
+        k_1 = temp
+        sliderk1, sliderk2 = temp, temp
+        clicks_k2 = 0
 
     exp_1 = (1 / mu_1) ** t_range
     if k_2 > k_1:
@@ -242,7 +289,7 @@ def update_graph(e, k_1, k_2, a, y_1i, y_2i, clicks_bg):
     # Define the figure layout
     layout = go.Layout(title=None,
                        xaxis=dict(title=None),
-                       yaxis=dict(title='Values', range=[-4, 20]),
+                       yaxis=dict(title=None, range = [-4,20]),
                        showlegend=True, dragmode='pan')
     
     #Defining the lines that eventually show up on the graph based on the traces defined using the equations above
@@ -259,7 +306,11 @@ def update_graph(e, k_1, k_2, a, y_1i, y_2i, clicks_bg):
     zerolinewidth=2,
     zerolinecolor='black'
 )
-    return fig, clicks_bg
+    #resets the view to a reasonable range of view, default
+    if clicks_rv > 0:
+        fig['layout']['yaxis']['range'] = [-4,20]
+        clicks_rv = 0
+    return fig, clicks_k1, clicks_k2, sliderk1, sliderk2
 
 
 if __name__ == '__main__':
