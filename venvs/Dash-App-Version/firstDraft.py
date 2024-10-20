9d96a54c64aa36c379576d7c62a4aa0dcb829dba
vanschal <vanschal@bc.edu>
2024-10-20 10:43:31 -0400

    new changes

--- venvs/Dash-App-Version/firstDraft.py
+++ venvs/Dash-App-Version/firstDraft.py
@@ -1,26 +1,9 @@
 import dash
 from dash import dcc, html
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 import numpy as np
 
-#23/8/2024
-#I have made some progress since the last update, fixed the dragging, pinching, zooming of the graph
-#Added the x axis line and the input boxes of each variable to enter the value manually
-#Reset view button and the balanced growth button was also added
-#In the next draft I aim to link the input boxes and the slider values so that they correspond and match up with one another
-#The reset view button and balanced growth button will also be made functional
-
-#27/8/2024
-#I have the balanced growth button working now
-#need some clarification on the reset view button and what it has to achieve exactly
-#some help linking the input boxes and the slider values would also help greatly
-
-#28/8/2024
-#I improved the growth path buttons to make it more relevant 
-#gives more options for the user to either change k_1 or k_2 depending on what they want
-#The reset view button also works now
-#only obvious thing left for me to consider is now linking the input buttons to the slider values and vice versa
 
 app = dash.Dash(__name__)
 
@@ -85,8 +68,26 @@ app.layout = html.Div([
             html.Div([
             html.Label("Initial Value Output for Department 1 y_1i:"),
             dcc.Slider(id='slider-y1i', min=0, max=10, step=None, value=1, marks=None, tooltip={"placement": "bottom", "always_visible": True}),
-            dcc.Input(id='input-y1i', type='number', min=0, max=10, step=0.1, value=1, style={'marginLeft': '2px', 'width': '40px'})
-             ],style={'alignItems': 'center'}),
+            dcc.Input(id='input-y1i', type='number', min=0, max=10, step=0.1, value=1, style={'marginLeft': '2px', 'width': '40px'}),
+              html.Button(
+                'Balanced Growth y1i', 
+                id='balanced-growth-button-y2i',
+                n_clicks=0,
+                style={
+                    'backgroundColor': '#28a745',  # Green background
+                    'color': 'white',  # White text
+                    'border': 'none',  # No border
+                    'padding': '10px 20px',  # Padding
+                    'textAlign': 'center',
+                    'textDecoration': 'none', 
+                    'display': 'inline-block',
+                    'fontSize': '16px',  # Font size
+                    'margin': '10px 0',  # Margin
+                    'cursor': 'pointer',  # Pointer cursor on hover
+                    'borderRadius': '5px',  # Rounded corners
+                }
+            )
+             ]),
 
 
              #slider and input box for initial value output y2i
@@ -94,51 +95,31 @@ app.layout = html.Div([
             html.Div([
             html.Label("Initial Value Output for Department 2 y_2i:"),
             dcc.Slider(id='slider-y2i', min=0, max=10, step=None, value=1, marks=None, tooltip={"placement": "bottom", "always_visible": True}),
-            dcc.Input(id='input-y2i', type='number', min=0, max=10, step=0.1, value=1, style={'marginLeft': '2px', 'width': '40px'})
-             ],style={'alignItems': 'center'}),
+            dcc.Input(id='input-y2i', type='number', min=0, max=10, step=0.1, value=1, style={'marginLeft': '2px', 'width': '40px'}), 
+             html.Button(
+                'Balanced Growth y2i', 
+                id='balanced-growth-button-y1i',
+                n_clicks=0,
+                style={
+                    'backgroundColor': '#28a745',  # Green background
+                    'color': 'white',  # White text
+                    'border': 'none',  # No border
+                    'padding': '10px 20px',  # Padding
+                    'textAlign': 'center',
+                    'textDecoration': 'none', 
+                    'display': 'inline-block',
+                    'fontSize': '16px',  # Font size
+                    'margin': '10px 0',  # Margin
+                    'cursor': 'pointer',  # Pointer cursor on hover
+                    'borderRadius': '5px',  # Rounded corners
+                }
+            ), 
+             ]),
 
 
                 #this is the button to achieve balanced growth
                 #the eventual function of this is to set k1 and k2 equal to one another
                 #balanced growth, just like in the desmos app
-             html.Button(
-                'Balanced Growth K1 (adjusts K2 to equal K1)', 
-                id='balanced-growth-button-k1',
-                n_clicks=0,
-                style={
-                    'backgroundColor': '#28a745',  # Green background
-                    'color': 'white',  # White text
-                    'border': 'none',  # No border
-                    'padding': '10px 20px',  # Padding
-                    'textAlign': 'center',
-                    'textDecoration': 'none', 
-                    'display': 'inline-block',
-                    'fontSize': '16px',  # Font size
-                    'margin': '10px 0',  # Margin
-                    'cursor': 'pointer',  # Pointer cursor on hover
-                    'borderRadius': '5px',  # Rounded corners
-                }
-            ),
-
-                  html.Button(
-                'Balanced Growth K2 (adjusts K1 to equal K2)', 
-                id='balanced-growth-button-k2',
-                n_clicks=0,
-                style={
-                    'backgroundColor': '#28a745',  # Green background
-                    'color': 'white',  # White text
-                    'border': 'none',  # No border
-                    'padding': '10px 20px',  # Padding
-                    'textAlign': 'center',
-                    'textDecoration': 'none', 
-                    'display': 'inline-block',
-                    'fontSize': '16px',  # Font size
-                    'margin': '10px 0',  # Margin
-                    'cursor': 'pointer',  # Pointer cursor on hover
-                    'borderRadius': '5px',  # Rounded corners
-                }
-            ),
-
 
                 #this is the button to reset the viewport of the graph
             html.Button(
@@ -187,33 +168,59 @@ app.layout = html.Div([
 #  When a value changes the callback will be activated to update what the graph looks like according to the new values
 @app.callback(
     Output('graph', 'figure'),
-    Output('balanced-growth-button-k1', 'n_clicks'),
-    Output('balanced-growth-button-k2', 'n_clicks'),
-    #giving it an output so that it can be reset to 0 as described below
-    #this is here because of the balanced growth button to link them up and reflect it on the slider value
-    Output('slider-k1','value'),
-    Output('slider-k2','value'),
 
+    Output('balanced-growth-button-y1i', 'n_clicks'),
+    Output('balanced-growth-button-y2i', 'n_clicks'),
+
+    Output('slider-y1i', 'value'),
+    Output('slider-y2i', 'value'),
+
+    Output('slider-e', 'value'),
+    Output('input-e', 'value'),
+
+    Output('slider-k1', 'value'),
+    Output('input-k1', 'value'),
+
+    Output('slider-k2', 'value'),
+    Output('input-k2', 'value'),
+
+    Output('slider-a','value'),
+    Output('input-a','value'),
 
     #below are the values used to get the initial graph by feeding them into the equations
     Input('slider-e','value'),
     Input('slider-k1', 'value'),
     Input('slider-k2', 'value'),
     Input('slider-a', 'value'),
+
     Input('slider-y1i', 'value'),
     Input('slider-y2i', 'value'),
-    Input('balanced-growth-button-k1', 'n_clicks'),
-    Input('balanced-growth-button-k2', 'n_clicks'),
-    Input('reset-view-button','n_clicks')
-    #tracking if balanced growth path button has been clicked
-    # Input('reset-view-button', 'n_clicks') #tracking if rest view button has been clicked
+    
+    Input('balanced-growth-button-y1i', 'n_clicks'),
+    Input('balanced-growth-button-y2i', 'n_clicks'),
+
+    Input('slider-e','value'),
+    Input('input-e','value'),
+    
+    Input('slider-k1', 'value'),
+    Input('input-k1', 'value'),
+
+    Input('slider-k2', 'value'),
+    Input('input-k2', 'value'),
+
+    Input('slider-a','value'),
+    Input('input-a','value')
 )
 
 
 # Update graph function, basically copied off of the tkinter version
-def update_graph(e, k_1, k_2, a, y_1i, y_2i, clicks_k1, clicks_k2, clicks_rv):
+def update_graph(e, k_1, k_2, a, y_1i, y_2i, bg1clicks, bg2clicks, slider_e, input_e, slider_k1, input_k1, slider_k2, input_k2, slider_a, input_a):
+    ctx = dash.callback_context
+    trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
 
 
+    slidery1i = y_1i
+    slidery2i = y_2i
 
     c_1 = k_1 / (1 + e + k_1)
     v_1 = (1 - c_1) / (1 + e)
@@ -235,6 +242,20 @@ def update_graph(e, k_1, k_2, a, y_1i, y_2i, clicks_k1, clicks_k2, clicks_rv):
     m_12 = (mu_1 - M_11) / M_12 * m_11
     m_21 = 1
     m_22 = -(M_11 - mu_2) / M_12 * m_21
+    bg1 = (m_12/m_11)*y_1i
+    bg2 = (m_11/m_12)*y_2i
+
+    if bg1clicks > 0:
+        y_2i = bg1
+        slidery2i = bg1
+        bg1clicks = 0
+
+    if bg2clicks > 0:
+        y_1i = bg2
+        slidery1i = bg2
+        bg2clicks = 0
+
+
     P = np.array([[m_11, m_21], [m_12, m_22]])
     P_inverse = np.linalg.inv(P)
     y_vec = np.array([[y_1i], [y_2i]])
@@ -244,8 +265,18 @@ def update_graph(e, k_1, k_2, a, y_1i, y_2i, clicks_k1, clicks_k2, clicks_rv):
 
     t_range = np.linspace(-3, 14, 1000)
 
-    sliderk1 = k_1
-    sliderk2 = k_2
+    inpute = input_e if trigger_id == 'input-e' else slider_e
+    slidere = slider_e if trigger_id == 'slider-e' else input_e
+
+    inputk1 = input_k1 if trigger_id == 'input-k1' else slider_k1
+    sliderk1 = slider_k1 if trigger_id == 'slider-k1' else input_k1
+
+    inputk2 = input_k2 if trigger_id == 'input-k2' else slider_k2
+    sliderk2 = slider_k2 if trigger_id == 'slider-k2' else input_k2
+
+    inputa = input_a if trigger_id == 'input-a' else slider_a
+    slidera = slider_a if trigger_id == 'slider-a' else input_a
+
 
     #sets the initial value for the sliders to be returned later in case the user doesn't click the growth path buttons at all
 
@@ -255,18 +286,6 @@ def update_graph(e, k_1, k_2, a, y_1i, y_2i, clicks_k1, clicks_k2, clicks_rv):
 
     #sets one value on to the other depending on which was clicked on the app below
     #different if clause is activated depending on k1 or k2 click
-
-    if clicks_k1 > 0:
-        temp = k_1
-        k_2 = temp
-        sliderk2, sliderk1 = temp, temp
-        clicks_k1 = 0
-    
-    if clicks_k2 > 0:
-        temp = k_2
-        k_1 = temp
-        sliderk1, sliderk2 = temp, temp
-        clicks_k2 = 0
 
     exp_1 = (1 / mu_1) ** t_range
     if k_2 > k_1:
@@ -288,7 +307,7 @@ def update_graph(e, k_1, k_2, a, y_1i, y_2i, clicks_k1, clicks_k2, clicks_rv):
 
     # Define the figure layout
     layout = go.Layout(title=None,
-                       xaxis=dict(title=None),
+                       xaxis=dict(),
                        yaxis=dict(title=None, range = [-4,20]),
                        showlegend=True, dragmode='pan')
     
@@ -307,11 +326,16 @@ def update_graph(e, k_1, k_2, a, y_1i, y_2i, clicks_k1, clicks_k2, clicks_rv):
     zerolinecolor='black'
 )
     #resets the view to a reasonable range of view, default
-    if clicks_rv > 0:
-        fig['layout']['yaxis']['range'] = [-4,20]
-        clicks_rv = 0
-    return fig, clicks_k1, clicks_k2, sliderk1, sliderk2
+    return fig, bg1clicks, bg2clicks, slidery1i, slidery2i, slidere, inpute, sliderk1, inputk1, sliderk2, inputk2, slidera, inputa
 
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+
+#the main problems that I am experiencing 
+# 1. when I don't touch the sliders for k_1 k_2 they change based on the values of initial output of each department
+# 2. Extending the graph infinitely and keeping it scaled
+# 3. When the user exits the input box there is a division by zero error a lot of the time
+# 4. How do I split my code into separate files?
+# 5. What other stuff should I add to make it more complete?
